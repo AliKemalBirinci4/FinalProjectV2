b2a3628a8667fc3ab2d4e3250246effa7f2e9e47
benpıroyum <birincialikemal18@gmail.com>
2025-08-07 13:30:45 +0300

    9 bitti

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,20 +12,42 @@ public class Program
     {
         var context = new NorthwindContext();
 
- 
 
 
 
-        ProductManager productManager = new ProductManager(new EfProductDal());
 
-        foreach (var product in productManager.GetByUnitPrice(40,100))
-        {
-            Console.WriteLine(product.ProductName);
-        }
 
-        Console.WriteLine("Can not get");
 
+
+
+
+
+
+
+
+       ProductTest();
+
+        //CategoryTest();
 
     }
-  
+
+    private static void CategoryTest()
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+        foreach (var category in categoryManager.GetAll())
+        {
+            Console.WriteLine(category.CategoryName);
+        }
+    }
+
+    private static void ProductTest()
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.GetProductDetails())
+        {
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+        }
+    }
 }
